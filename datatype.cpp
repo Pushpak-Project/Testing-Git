d559b5b5afc86383f7d94faba3b1b2846b8dcc81
Pushpak-Project <pushpakyele0@gmail.com>
2024-02-11 02:16:45 +0530

    This is my first commit

---
+++ datatype.cpp
@@ -0,0 +1,27 @@
+// These are imports
+#include <iostream>
+#include <iomanip>
+#include<limits>
+using namespace std;
+
+int main()
+{
+    int a = 122543;
+
+    float b = 12.24556346f; // 7 signi digits
+
+    double c = 1247.25248785185135168; // 15 signi digits
+
+    long double d = 457.247456455315648453324L; // 18 signi digits
+
+    // cout gives 6 signi digits
+    // Hey this is for test 
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<setprecision(10)<<c<<endl;
+    cout<<setprecision(16)<<d<<endl;
+
+    cout<<numeric_limits<float>::digits10<<endl;
+
+}
